5741dcbf4f0afd89e63b5c20d70e8c75e6597eaa
Metalblueberry <metalblueberry@gmail.com>
2019-11-01 12:21:00 +0100

    add test for complex area

--- mandelbrot/area_test.go
+++ mandelbrot/area_test.go
@@ -14,9 +14,25 @@ func BenchmarkArea(b *testing.B) {
 		TopLeft:              complex(-2, 2),
 		BottomRight:          complex(2, -2),
 	}
+	benchmarkGivenArea(b, set)
+}
+
+func BenchmarkComplexArea(b *testing.B) {
+	set := mandelbrot.Area{
+		HorizontalResolution: 1060,
+		VerticalResolution:   730,
+		MaxIterations:        3534,
+		TopLeft:              complex(-1.401854499759, -0.000743603637),
+		BottomRight:          complex(-1.399689899172, 0.000743603637),
+	}
+	benchmarkGivenArea(b, set)
+}
+
+func benchmarkGivenArea(b *testing.B, set mandelbrot.Area) {
 	set.Init()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		set.Calculate()
 	}
+
 }
