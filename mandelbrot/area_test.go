61e9e9934a9ff38f7a79f48511393cb9950976e4
Metalblueberry <metalblueberry@gmail.com>
2019-11-01 08:37:42 +0100

    add benchmark

---
+++ mandelbrot/area_test.go
@@ -0,0 +1,24 @@
+package mandelbrot_test
+
+import (
+	"testing"
+
+	"github.com/metalblueberry/mandelbrot/mandelbrot"
+)
+
+func BenchmarkArea(b *testing.B) {
+	set := mandelbrot.Area{
+		HorizontalResolution: 200,
+		VerticalResolution:   200,
+		MaxIterations:        200,
+		TopLeft:              complex(-2, 2),
+		BottomRight:          complex(2, -2),
+	}
+	set.Init()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		progress := set.Calculate()
+		for range progress {
+		}
+	}
+}
