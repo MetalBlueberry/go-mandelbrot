886e0bf25cf7a1e4075da16d9e0197284be10e97
Metalblueberry <metalblueberry@gmail.com>
2019-11-17 19:19:00 +0100

    fixed area benchmarks

--- mandelbrot/area_test.go
+++ mandelbrot/area_test.go
@@ -7,32 +7,33 @@ import (
 )
 
 func BenchmarkArea(b *testing.B) {
-	set := mandelbrot.Area{
-		HorizontalResolution: 200,
-		VerticalResolution:   200,
-		MaxIterations:        200,
-		TopLeft:              complex(-2, 2),
-		BottomRight:          complex(2, -2),
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		set := mandelbrot.Area{
+			HorizontalResolution: 200,
+			VerticalResolution:   200,
+			MaxIterations:        200,
+			TopLeft:              complex(-2, 2),
+			BottomRight:          complex(2, -2),
+		}
+		set.Init()
+		b.StartTimer()
+		set.Calculate()
 	}
-	benchmarkGivenArea(b, set)
 }
 
 func BenchmarkComplexArea(b *testing.B) {
-	set := mandelbrot.Area{
-		HorizontalResolution: 1060,
-		VerticalResolution:   730,
-		MaxIterations:        3534,
-		TopLeft:              complex(-1.401854499759, -0.000743603637),
-		BottomRight:          complex(-1.399689899172, 0.000743603637),
-	}
-	benchmarkGivenArea(b, set)
-}
-
-func benchmarkGivenArea(b *testing.B, set mandelbrot.Area) {
-	set.Init()
-	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		set := mandelbrot.Area{
+			HorizontalResolution: 1060,
+			VerticalResolution:   730,
+			MaxIterations:        3534,
+			TopLeft:              complex(-1.401854499759, -0.000743603637),
+			BottomRight:          complex(-1.399689899172, 0.000743603637),
+		}
+		set.Init()
+		b.StartTimer()
 		set.Calculate()
 	}
-
 }
